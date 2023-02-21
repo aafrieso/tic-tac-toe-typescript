a2f24e86b63bd9eb371aec12a62a76434f951087
Austin Friesorger <aafrieso@svsu.edu>
2023-02-21 10:15:34 -0500

    add constants

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const squareEls = document.querySelectorAll('div');
+const messageEls = document.getElementById('message') as HTMLElement;
+const resetBtnEl = document.querySelector('button') as HTMLButtonElement;
+
+const winningCombos: number[][] = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+
+/*-------------------------------- Functions --------------------------------*/
